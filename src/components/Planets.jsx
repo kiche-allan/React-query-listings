cc595ace40d94dfb581e91d899c1c95f2eadeb10
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-12 10:07:42 +0300

    planet3

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -3,12 +3,23 @@ import { useQuery } from 'react-query';
 
 const fetchPlanets = async () => {
     const res = await fetch('http://swapi.dev/api/planets/');
+    return res.json();
 }
 
 export default function Planets() {
 
-    const {} = useQuery('planets', fetchPlanets);
+    const { data, status} = useQuery('planets', fetchPlanets);
+    
   return (
-    <div><h2>Planets</h2></div>
+    <div><h2>Planets</h2>
+    {/* <p>{status}</p> */}
+
+    {status === 'loading' && (
+        <div>Loading data</div>
+    )}
+    {status === 'error' && (
+        <div>Error fetching data</div>
+    )}
+    </div>
   )
 }
